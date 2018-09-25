f80ce1dcc6794f0fc295bb7b4248217cb0c5b082
gittindeep <cloudboy@usa.com>
2018-09-25 12:50:57 -0400

    created Driver class

---
+++ javapractice/src/com/practice/one/Driver.java
@@ -0,0 +1,11 @@
+package com.practice.one;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Uploaded to Github");
+	}
+
+}
